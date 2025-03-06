17deb911c0d53938e2d70a875dd63e25e0240760
Rediet Haileab <wegenerediet@gmail.com>
2025-03-06 13:08:06 +0300

    modification

--- Backend/app/utility/jwt_auth.py
+++ Backend/app/utility/jwt_auth.py
@@ -6,7 +6,7 @@ import jwt
 # Secret & Hashing Configurations
 SECRET_KEY = "mysecretkey"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_Days = 30
+ACCESS_TOKEN_EXPIRE_Days = 1
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -15,7 +15,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Token Creation Function
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=15))
+    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(days=ACCESS_TOKEN_EXPIRE_Days))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     print(f"Token created: {encoded_jwt}")  # Log the created token
